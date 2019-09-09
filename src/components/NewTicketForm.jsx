ceea15991909354d5e329690f3bdfe826a06bcba
Student <example@example.com>
2019-09-09 14:31:33 -0700

    Add redirect to home after new ticket is submitted.

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,41 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
+import { Route, Redirect } from 'react-router-dom';
 
-function NewTicketForm(props){
-  let _names = null;
-  let _location = null;
-  let _issue = null;
-
-  function handleNewTicketFormSubmission(event) {
-    event.preventDefault();
-    props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4()});
-    _names.value = '';
-    _location.value = '';
-    _issue.value = '';
+class NewTicketForm extends React.Component {
+  constructor(props){
+    super(props)
+    this._names = null;
+    this._location = null;
+    this._issue = null;
+    this.state = {
+      redirectToHome: false
+    };
+    this.handleNewTicketFormSubmission = this.handleNewTicketFormSubmission.bind(this);
   }
 
-  return (
-    <div>
-      <form onSubmit={handleNewTicketFormSubmission}>
-        <input
-          type='text'
-          id='names'
-          placeholder='Pair Names'
-          ref={(input) => {_names = input;}}/>
-        <input
-          type='text'
-          id='location'
-          placeholder='Location'
-          ref={(input) => {_location = input;}}/>
-        <textarea
-          id='issue'
-          placeholder='Describe your issue.'
-          ref={(textarea) => {_issue = textarea;}}/>
-        <button type='submit'>Help!</button>
-      </form>
-    </div>
-  );
+  handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    this.props.onNewTicketCreation({names: this._names.value, location: this._location.value, issue: this._issue.value, id: v4()});
+    this._names.value = '';
+    this._location.value = '';
+    this._issue.value = '';
+    this.setState({redirectToHome: true})
+  }
+
+  render() {
+    if (this.state.redirectToHome) {
+      return <Redirect to='/' />
+    }
+    return (
+      <div>
+        <form onSubmit={this.handleNewTicketFormSubmission}>
+          <input
+            type='text'
+            id='names'
+            placeholder='Pair Names'
+            ref={(input) => {this._names = input;}}/>
+          <input
+            type='text'
+            id='location'
+            placeholder='Location'
+            ref={(input) => {this._location = input;}}/>
+          <textarea
+            id='issue'
+            placeholder='Describe your issue.'
+            ref={(textarea) => {this._issue = textarea;}}/>
+          <button type='submit'>Help!</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 NewTicketForm.propTypes = {
